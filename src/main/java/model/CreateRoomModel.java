a2bf0244e9a1b5c38da7b20374a8d9dc8067e678
s20223073 <s20223073@host.docker.internal>
2023-10-26 12:06:27 +0900

    CreateRoomServlet 更新

--- src/main/java/model/CreateRoomModel.java
+++ src/main/java/model/CreateRoomModel.java
@@ -1,5 +1,23 @@
 package model;
 
+import bean.User;
+import dao.RoomDAO;
+import exception.SwackException;
+
 public class CreateRoomModel {
 
+	public boolean getusername(User user) throws SwackException {
+		//				System.out.println("[getChatlogList] " + roomId);
+		//		ArrayList<Room> list = new ArrayList<Room>();
+		//		list.add(new Room("U0001", "ダミー太郎"));
+		//		list.add(new Room("U0002", "ダミー花子"));
+		//		list.add(new Room("U0003", "ダミー次郎"));
+		//		list.add(new Room("U0004", "ダミー三郎"));
+		//		list.add(new Room("U0005", "ダミー四郎"));
+
+		RoomDAO roomDAO = new RoomDAO();
+		boolean result = roomDAO.insert(user);
+
+		return result;
+	}
 }
