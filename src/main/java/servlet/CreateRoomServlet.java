525497843b0a83f688b9cf6294e16a9180c854c1
s20223073 <s20223073@host.docker.internal>
2023-10-27 14:09:08 +0900

    CreateRoomServlet doGet完成

--- src/main/java/servlet/CreateRoomServlet.java
+++ src/main/java/servlet/CreateRoomServlet.java
@@ -1,12 +1,20 @@
 package servlet;
 
+import static parameter.Messages.*;
+
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import bean.User;
+import exception.SwackException;
+import model.RoomModel;
+import model.UserModel;
 
 /**
  * Servlet implementation class CreateRoomServlet
@@ -28,8 +36,18 @@ public class CreateRoomServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		UserModel usermodel = new UserModel();
+		try {
+			ArrayList<User> userlist = usermodel.getUsers();
+			request.setAttribute("userList", userlist);
+			request.getRequestDispatcher("/webapp/Mycreateroom.jsp").forward(request, response);
+		} catch (SwackException e) {
+			e.printStackTrace();
+			request.setAttribute("errorMsg", ERR_SYSTEM);
+			request.getRequestDispatcher("/webapp/Mycreateroom.jsp").forward(request, response);
+			return;
+		}
 
-		request.getRequestDispatcher("/webapp/Mycreateroom.jsp").forward(request, response);
 	}
 
 	/**
@@ -40,8 +58,13 @@ public class CreateRoomServlet extends HttpServlet {
 
 		// パラメータ取得
 		String roomname = request.getParameter("roomName");
-		String joinusers = request.getParameter("joinUsers");
-		String directed = request.getParameter("directed");
+		String joinuserid = request.getParameter("joinUserId");
+		String directed = request.getParameter("privated");
+		//セッションからuserを取得する
+
+		//GetIdModelからroomIdを取得する
+		RoomModel roommodel = new RoomModel();
+		roomModel = RoomModel.createRoom(roomId, roomName, user.getUserId(), directed, privated);
 
 	}
 
