c94f67fe6accbeb9be1ece284a871207c92922c6
s20223005 <s20223005@host.docker.internal>
2023-11-14 11:30:06 +0900

    編集機能完成

--- src/main/webapp/js/editchat.js
+++ src/main/webapp/js/editchat.js
@@ -1,10 +1,15 @@
 "use strict";
 
 function editChat(chatId, target, userId) {
-	
-	if(target != userId){
+	if (target != userId) {
 		alert("あなたのチャットではありません");
 		return;
 	}
-	document.getElementById()
+	console.log(String(chatId));
+	const targets = document.getElementsByClassName(String(chatId));
+	targets[1].value = targets[0].textContent;
+	for (const target of targets) {
+		target.classList.toggle('active');
+	}
 }
+
