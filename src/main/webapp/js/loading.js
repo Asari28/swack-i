c7ee8f3d6d1cd35b07e519cb597f2a1b5bd239e6
s20223005 <s20223005@host.docker.internal>
2023-11-28 09:45:55 +0900

    機能修正

--- src/main/webapp/js/loading.js
+++ src/main/webapp/js/loading.js
@@ -2,10 +2,11 @@
 
 const form = document.getElementById("form");
 const msg = document.getElementById("msg");
-const msgList = ["Loading now...","ローディング中...","こんにちは！","ようこそ！"]
+const msgList = ["Loading now...", "ローディング中...", "こんにちは！", "ようこそ！", "お久しぶりです！"]
 
 document.addEventListener('DOMContentLoaded',() => {
-
+	const index = Math.floor(Math.random() * msgList.length);
+	msg.textContent = msgList[index];
 });
 //ログイン成功時２秒後にメイン画面へ
 setTimeout(function(){
